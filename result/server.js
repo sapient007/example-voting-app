cfb2ec858a3c6b238b6d1c00b017b63ae80b65e3
Ming Ling <mingling@mingling-macbookpro.roam.corp.google.com>
2020-03-12 23:38:33 -0400

    updated to use bigquery

--- result/server.js
+++ result/server.js
@@ -23,51 +23,95 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-var pool = new pg.Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
-});
+// var pool = new pg.Pool({
+//   connectionString: 'postgres://postgres:postgres@db/postgres'
+// });
 
-async.retry(
-  {times: 1000, interval: 1000},
-  function(callback) {
-    pool.connect(function(err, client, done) {
-      if (err) {
-        console.error("Waiting for db");
-      }
-      callback(err, client);
+// async.retry(
+//   {times: 1000, interval: 1000},
+//   function(callback) {
+//     pool.connect(function(err, client, done) {
+//       if (err) {
+//         console.error("Waiting for db");
+// ``      }
+//       callback(err, client);
+//     });
+//   },
+//   function(err, client) {
+//     if (err) {
+//       return console.error("Giving up");
+//     }
+//     console.log("Connected to db");
+//     getVotes(client);
+//   }
+// );
+
+
+// Import the Google Cloud client library
+const {BigQuery} = require('@google-cloud/bigquery');
+
+async function queryVotes() {
+// Queries a public Shakespeare dataset.
+
+    // Create a client
+    const bigqueryClient = new BigQuery();
+    console.log('created bigquery client');
+
+    // The SQL query to run
+    const sqlQuery = `SELECT vote_selection, count(voter_id) AS count FROM \`automl-document-mling.votes.votes\` GROUP BY vote_selection`;
+
+    const options = {
+    query: sqlQuery,
+    // Location must match that of the dataset(s) referenced in the query.
+    location: 'US',
+    };
+
+    // Run the query
+    const [rows] = await bigqueryClient.query(options);
+    
+    var votes = {a: 0, b: 0};
+    
+    console.log('Rows:');
+    // rows.forEach(row => {
+    //   const url = row['vote_selection'];
+    //   const viewCount = row['count'];
+    //   votes[row.vote_selection] = parseInt(row.count);
+    //   console.log(`url: ${url}, ${viewCount} views`);
+    // });
+
+    rows.forEach(function(row){
+      votes[row.vote_selection] = parseInt(row.count);
     });
-  },
-  function(err, client) {
-    if (err) {
-      return console.error("Giving up");
-    }
-    console.log("Connected to db");
-    getVotes(client);
-  }
-);
 
-function getVotes(client) {
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
-    if (err) {
-      console.error("Error performing query: " + err);
-    } else {
-      var votes = collectVotesFromResult(result);
-      io.sockets.emit("scores", JSON.stringify(votes));
-    }
-
-    setTimeout(function() {getVotes(client) }, 1000);
-  });
+    console.log(JSON.stringify(votes));
+    io.sockets.emit("scores", JSON.stringify(votes));
 }
 
-function collectVotesFromResult(result) {
-  var votes = {a: 0, b: 0};
+queryVotes();
 
-  result.rows.forEach(function (row) {
-    votes[row.vote] = parseInt(row.count);
-  });
 
-  return votes;
-}
+// function getVotes(client) {
+//   client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
+//     if (err) {
+//       console.error("Error performing query: " + err);
+//     } else {
+//       var votes = collectVotesFromResult(result);
+//       io.sockets.emit("scores", JSON.stringify(votes));
+//     }
+
+//     setTimeout(function() {getVotes(client) }, 1000);
+//   });
+// }
+
+// function collectVotesFromResult(result) {
+//   var votes = {a: 0, b: 0};
+
+//   result.rows.forEach(function (row) {
+//     votes[row.vote] = parseInt(row.count);
+//   });
+
+//   return votes;
+// }
 
 app.use(cookieParser());
 app.use(bodyParser());
